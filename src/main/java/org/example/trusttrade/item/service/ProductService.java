4c6f6fceded625a1fb0ffd4e2a11edc14e299251
hanjh <wg1004s@naver.com>
2025-10-31 18:46:29 +0900

    feat: 지도 기능 및 api 수정

--- src/main/java/org/example/trusttrade/item/service/ProductService.java
+++ src/main/java/org/example/trusttrade/item/service/ProductService.java
@@ -4,38 +4,70 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.trusttrade.item.domain.products.Product;
+import org.example.trusttrade.item.dto.StoredImage;
 import org.example.trusttrade.item.dto.request.BasicItemDto;
-import org.example.trusttrade.item.repository.ItemRepository;
+import org.example.trusttrade.item.dto.response.ItemResponseDto;
+import org.example.trusttrade.item.dto.response.ProductResponseDto;
 import org.example.trusttrade.item.repository.ProductRepository;
 import org.example.trusttrade.login.domain.User;
-import org.example.trusttrade.login.service.UserService;
-
-import org.example.trusttrade.login.repository.UserRepository;
-
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductService {
 
-    private final ItemService itemService;
-    private final UserService userService;
+    private final ImageService imageService;
     private final ProductRepository productRepository;
-    private final ItemRepository itemRepository;
-    private final UserRepository userRepository;
 
+
+    // 일반 물품 등록
     @Transactional
-    public void registerProduct(BasicItemDto dto, User seller) {
-        // 1) Product 객체 생성
-        Product product = Product.fromDto(dto, seller);
-        log.debug("Product 객체 생성 완료: {}", product);
-        productRepository.save(product);
-        log.debug("Product 저장 완료: itemId={}", product.getId());
+    public void registerBasicItem(BasicItemDto basicItemDto, User seller, List<MultipartFile> images) {
+        List<StoredImage> storedImages = Collections.emptyList();
+        try {
 
-        // 2) 이미지·카테고리 저장
-        itemService.saveItemDetails(product, dto.getImages(), dto.getCategoryIds());
-        log.debug("이미지·카테고리 저장 완료: itemId={}", product.getId());
+            // 이미지 변환
+            storedImages = imageService.processImagesAndSetDto(images, basicItemDto);
+
+            // Product 생성
+            Product product = Product.fromDto(basicItemDto, seller);
+            productRepository.save(product);
+
+        } catch (Exception e) {
+            // 업로드된 사진 삭제
+            imageService.deleteFiles(storedImages);
+            throw new RuntimeException("일반 물품 등록 중 오류 발생", e);
+        }
+    }
+
+    // 일반 물품 전체 조회
+    public List<ItemResponseDto> getBasicItems() {
+        List<Product> items = productRepository.findAll();
+        return items.stream()
+                .map(ItemResponseDto::fromProduct)
+                .collect(Collectors.toList());
+    }
+
+
+    // 5km 이내의 물품 조회
+    public List<ProductResponseDto> findProductsNearby(double lat, double lng) {
+        List<Product> products = productRepository.findNearby(lat, lng);
+        return products.stream()
+                .map(product -> ProductResponseDto.builder()
+                        .item_id(product.getId())
+                        .sellerAccount(product.getUser().getUserAccount()) // 판매자명 대신 계정 사용
+                        .title(product.getName())
+                        .price(product.getProductPrice())
+                        .latitude(product.getProductLocation().getLatitude())
+                        .longitude(product.getProductLocation().getLongitude())
+                        .build())
+                .collect(Collectors.toList());
     }
 
 }
